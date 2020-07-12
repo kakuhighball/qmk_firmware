484af8f62bdbfc974a00dccb618bd7167ff7814b
kakuhighball <gatrhaze@gmail.com>
2020-07-12 09:45:12 -0700

    Add/Edit keymaps

--- keyboards/exclusive/e65/config.h
+++ keyboards/exclusive/e65/config.h
@@ -20,7 +20,7 @@
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID    0xFEED
-#define PRODUCT_ID   0x6060
+#define PRODUCT_ID   0x6500
 #define DEVICE_VER   0x0001
 #define MANUFACTURER Exclusive / E-Team
 #define PRODUCT      E6.5
